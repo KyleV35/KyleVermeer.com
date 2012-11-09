9a9451ad7ad9b3fdd29d3f4e532a37032040f154
Kyle Vermeer <kvermeer@stanford.edu>
2012-11-08 23:12:39 -0800

    Redoing website

---
+++ handlers/kyle.py
@@ -0,0 +1,15 @@
+import webapp2
+import logging
+import datetime
+from handlers.base_handler import BaseHandler
+from webapp2_extras import json
+from models.familyMember import *
+from google.appengine.ext import db
+from google.appengine.api import memcache
+from util.memcacheHelper import safeMemcacheSet
+from util.template import jinja_environment
+
+class FrontPageHandler(BaseHandler):
+    def get(self):
+        template= jinja_environment.get_template('front_page.html')
+        self.response.out.write(template.render({}))
--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from webapp2_extras import routes
 routes= [
     #webapp2.Route(r'/', handler='main.MainHandler'),
     #For now
-    webapp2.Route(r'/',handler='handlers.family.FamilyMainPageHandler'),
+    webapp2.Route(r'/',handler='handlers.kyle.FrontPageHandler'),
     webapp2.Route(r'/family',handler='handlers.family.FamilyMainPageHandler'),
     webapp2.Route(r'/family/member/<familyMemberName>', handler='handlers.family.FamilyHandler'),
     routes.PathPrefixRoute(r'/family/videos', [
